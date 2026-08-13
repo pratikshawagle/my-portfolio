239c5748fcdebd7e5465b87c7a580eeea3ec943b
pratikshawagle <waglepratiksha03@gmail.com>
2026-08-13 11:39:34 +0545

    portfolio completed

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed=new Typed("#typed-text",{
+    strings:[
+        "Full stack Developer",
+        "UI/UX Designer"
+    ],
+    typespeed:80,
+    backspeed:50,
+    backDelay:1500,
+    startDelay:500,
+    loop:true,
+    showCursor:true,
+    cursorchar:"|"
+});
